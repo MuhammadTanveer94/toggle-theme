7e59e831d8e9798d8893f9413735311add8b456d
tanveer_ulhaq <tanveer.ulhaq@uniquesoftwaredev.com>
2023-07-05 13:19:02 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import { ThemeContext } from "./context/theme-context";
+
+function App() {
+  const { theme, toggle, dark } = React.useContext(ThemeContext);
+  return (
+    <div className="App">
+      <header
+        className="App-header"
+        style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
+      >
+        <button
+          type="button"
+          onClick={toggle}
+          style={{
+            backgroundColor: theme.backgroundColor,
+            color: theme.color,
+            outline: "none",
+          }}
+          data-testid="toggle-theme-btn"
+        >
+          Toggle to {!dark ? "Dark" : "Light"} theme
+        </button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
